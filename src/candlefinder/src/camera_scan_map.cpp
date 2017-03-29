df63e4d815aefff38048bc326917e055fc0f8507
Giselle Koo <gisellegk@gmail.com>
2017-03-29 18:21:47 -0400

    this maybe does the ray thing? set numRays to how many rays you want to do

--- src/candlefinder/src/camera_scan_map.cpp
+++ src/candlefinder/src/camera_scan_map.cpp
@@ -26,9 +26,39 @@ int main(int argc, char* argv[]){
   info.height = 1;
 
   while(ros::ok()) {
+    int row = robot_row;
+    int col = robot_col;
+    int robotIndex = info.width*robot_row+robot_col;
     std::vector<int8_t> m = map;
     std::vector<int8_t>  cam_scan(info.width*info.height, -1);
-    cam_scan[info.width*robot_row+robot_col] = 1;
+    cam_scan[robotIndex] = 100; //robot is here
+
+    /*
+    for x number of rays
+      create the linear function that models the ray
+      "walk" along that line, checking the value in map until you hit 100
+      if the value of map is 0, add a 1 to the cam_scan map
+      if the value of the map is 100 or -1 stop!!
+
+    */
+    int numRays = 4;
+    for(int ray = 0; ray < numRays; ray++){
+      int angle = (360 / numRays)* ray;
+      // 0 = 0, 1 = 90, 2=180, 3=270
+
+      float rise = sin(angle);
+      float run = cos(angle); // i think?
+      int currentPixel = robotIndex;
+      //double check that the robot's position is considered "unoccupied"
+
+      //FOR EACH PIXEL IN THIS LINE
+      for(int i = 1; m[currentPixel] == 0; i++) {
+        cam_scan[currentPixel] = 1; // 1 = scanned. i guess. idk.
+        currentPixel = robotIndex + (int)(i * (info.width*run + rise));
+        //idk if you can just add this shit together or what okay
+      }
+      //now move on to the next ray :)
+    }
 
     nav_msgs::OccupancyGrid c;
     c.data = cam_scan;
@@ -49,5 +79,4 @@ void savePose(const geometry_msgs::PoseStamped& msg){
   pose = msg.pose;
   robot_row = (int)((pose.position.y / info.resolution) + (info.height/2.0));
   robot_col = (int)((pose.position.x / info.resolution) + (info.width/2.0));
- 
 }
