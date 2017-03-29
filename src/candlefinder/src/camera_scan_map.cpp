a736b21e9c686c3d69f91864a28c83d337ec0d9e
Giselle Koo <gisellegk@gmail.com>
2017-03-29 00:49:53 -0400

    angle

--- src/candlefinder/src/camera_scan_map.cpp
+++ src/candlefinder/src/camera_scan_map.cpp
@@ -11,18 +11,6 @@ geometry_msgs::Pose pose;
 int robot_row = 0;
 int robot_col=0;
 
-
-void saveMap(const nav_msgs::OccupancyGrid& msg){
-  map = msg.data;
-  info = msg.info;
-}
-
-void savePose(const geometry_msgs::PoseStamped& msg){
-  pose = msg.pose;
-  robot_row = (int)((pose.position.y / info.resolution) + (info.height/2.0));
-  robot_col = (int)(((pose.position.x+1) / info.resolution) + (info.width/2.0));
-}
-
 int main(int argc, char* argv[]){
   ros::init(argc, argv, "camera_scan_map_node");
   ros::NodeHandle nh;
@@ -34,20 +22,33 @@ int main(int argc, char* argv[]){
   ros::Rate rate(10); //idk
   ROS_INFO_STREAM("heres the camera scan map or something");
 
-info.width = 1;
-info.height = 1;
+  info.width = 1;
+  info.height = 1;
 
   while(ros::ok()) {
- 	std::vector<int8_t> m = map;
-       std::vector<int8_t>  cam_scan(info.width*info.height, -1);
-       cam_scan[info.width*robot_row+robot_col] = 1;
+    std::vector<int8_t> m = map;
+    std::vector<int8_t>  cam_scan(info.width*info.height, -1);
+    cam_scan[info.width*robot_row+robot_col] = 1;
 
-   nav_msgs::OccupancyGrid c;
+    nav_msgs::OccupancyGrid c;
     c.data = cam_scan;
     c.info = info;
-   pub.publish(c);
-   ros::spinOnce();
-   rate.sleep();
+    pub.publish(c);
+    ros::spinOnce();
+    rate.sleep();
   }
 
+}
+
+void saveMap(const nav_msgs::OccupancyGrid& msg){
+  map = msg.data;
+  info = msg.info;
+}
+
+void savePose(const geometry_msgs::PoseStamped& msg){
+  pose = msg.pose;
+  double angle = asin(-2.0*(pose.orientation.x*pose.orientation.z - pose.orientation.w*pose.orientation.y));
+  ROS_INFO_STREAM(angle);
+  robot_row = (int)((pose.position.y / info.resolution) + (info.height/2.0));
+  robot_col = (int)(((pose.position.x+1) / info.resolution) + (info.width/2.0));
 }
