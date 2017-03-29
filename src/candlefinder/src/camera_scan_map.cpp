9fdbdebca5b159e15b5b05cfcf389acdc1850f61
Giselle Koo <gisellegk@gmail.com>
2017-03-29 00:28:48 -0400

    compiles now

--- src/candlefinder/src/camera_scan_map.cpp
+++ src/candlefinder/src/camera_scan_map.cpp
@@ -3,37 +3,13 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <nav_msgs/MapMetaData.h>
 
-int8[] map;
-MapMetaData info;
-PoseStamped pose;
+std::vector<int8_t> map;
+nav_msgs::MapMetaData info;
+geometry_msgs::Pose pose;
 
-int robot_row;
-int robot_col;
+int robot_row = 0;
+int robot_col=0;
 
-int main(int argc, char* argv[]){
-  ros::init(argc, argv, "camera_scan_map_node");
-  ros::NodeHandle nh;
-
-  ros::Publisher pub = nh.advertise<nav_msgs::OccupancyGrid>("camera_scan_map", 1000);
-  ros::Subscriber<nav_msgs::OccupancyGrid> sub("map", saveMap );
-  ros::Subscriber<geometry_msgs::PoseStamped> sub("slam_out_pose", savePose );
-
-  ros::Rate rate(10); //idk
-  ROS_INFO_STREAM("heres the camera scan map or something");
-
-  while(ros::ok()) {
-    int8[] m = map;
-    int8[] cam_scan = {-1};
-    cam_scan[info.width*robot_row+robot_col] = 1;
-
-    OccupancyGrid c;
-    c.data = cam_scan;
-    c.info = info;
-    pub.publish(c);
-
-  }
-
-}
 
 void saveMap(const nav_msgs::OccupancyGrid& msg){
   map = msg.data;
@@ -43,5 +19,34 @@ void saveMap(const nav_msgs::OccupancyGrid& msg){
 void savePose(const geometry_msgs::PoseStamped& msg){
   pose = msg.pose;
   robot_row = (int)((pose.position.y / info.resolution) + (info.height/2.0));
-  robot_col = (int)((pose.position.x / info.resolution) + (info.width/2.0));
+  robot_col = (int)(((pose.position.x+1) / info.resolution) + (info.width/2.0));
+}
+
+int main(int argc, char* argv[]){
+  ros::init(argc, argv, "camera_scan_map_node");
+  ros::NodeHandle nh;
+
+  ros::Publisher pub = nh.advertise<nav_msgs::OccupancyGrid>("camera_scan_map", 1000);
+  ros::Subscriber mapSub = nh.subscribe("map", 1000, &saveMap);
+  ros::Subscriber poseSub = nh.subscribe("slam_out_pose", 1000, &savePose);
+
+  ros::Rate rate(10); //idk
+  ROS_INFO_STREAM("heres the camera scan map or something");
+
+info.width = 1;
+info.height = 1;
+
+  while(ros::ok()) {
+ 	std::vector<int8_t> m = map;
+       std::vector<int8_t>  cam_scan(info.width*info.height, -1);
+       cam_scan[info.width*robot_row+robot_col] = 1;
+
+   nav_msgs::OccupancyGrid c;
+    c.data = cam_scan;
+    c.info = info;
+   pub.publish(c);
+   ros::spinOnce();
+   rate.sleep();
+  }
+
 }
