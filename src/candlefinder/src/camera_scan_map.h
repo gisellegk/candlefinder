9d15b70d97d9db0c1b38e15f87d72d607e1817aa
Giselle Koo <gisellegk@gmail.com>
2017-03-30 23:07:01 -0400

    oh yeah header files exist

--- src/candlefinder/src/camera_scan_map.h
+++ src/candlefinder/src/camera_scan_map.h
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Quaternion.h>
 #include <nav_msgs/MapMetaData.h>
 
 void saveMap(const nav_msgs::OccupancyGrid&);
 void savePose(const geometry_msgs::PoseStamped&);
+void saveAngle(const geometry_msgs::Quaternion&);
