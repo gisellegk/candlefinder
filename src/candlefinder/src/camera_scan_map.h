2f0e5911f5b72bb04154f22331a23eb1a324cef6
Giselle Koo <gisellegk@gmail.com>
2018-01-21 21:31:35 -0500

    hardware

--- src/candlefinder/src/camera_scan_map.h
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Quaternion.h>
-#include <nav_msgs/MapMetaData.h>
-
-void saveMap(const nav_msgs::OccupancyGrid&);
-void savePose(const geometry_msgs::PoseStamped&);
-void saveHeadAngle(const geometry_msgs::Quaternion&);
